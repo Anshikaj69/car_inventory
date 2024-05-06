43cbb2139e0e0415eb0362c6dfd68207fed7f6c8
your <jaiswalanshikaajay.7@gmail.com>
2024-05-06 19:12:18 +0530

    bug-fixes

--- server/index.js
+++ server/index.js
@@ -4,13 +4,9 @@ const cors = require('cors')
 const app= express()
 const PORT = 5001
 
-  
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://carinventoryanshika-anshikaj69s-projects.vercel.app");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
+app.use(cors({
+    origin : "https://carinventoryanshika-anshikaj69s-projects.vercel.app",
+}))
 
 
 app.use(express.json())
