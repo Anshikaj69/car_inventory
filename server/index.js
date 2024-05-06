cc7c623c006a65c709ebb84b26b84e3db3bad292
your <jaiswalanshikaajay.7@gmail.com>
2024-05-06 18:49:10 +0530

    bug-fixes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express= require('express')
-const cors= require('cors')
+const cors = require('cors')
 
 const app= express()
 const PORT = 5001
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://carinventoryanshika-anshikaj69s-projects.vercel.app/'); // Replace '*' with your frontend domain
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  
+app.use(cors());
 
 app.use(cors())
 app.use(express.json())
