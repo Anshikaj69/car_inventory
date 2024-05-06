ff4212392564a3479d3d0e80211275c0488c148c
your <jaiswalanshikaajay.7@gmail.com>
2024-05-06 20:12:12 +0530

    bug-fixes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express= require('express')
+const cors= require('cors')
 
 const app= express()
 const PORT = 5001
 
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'PUT'], 
+  }));
 
 app.use(express.json())
 
